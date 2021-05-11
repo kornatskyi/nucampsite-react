afa98fb6597b3d8dbc81acf20d1f3f7f4a791cb2
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-05-11 15:07:11 -0700

    Components Part 2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
 
 ReactDOM.render(
   <React.StrictMode>
